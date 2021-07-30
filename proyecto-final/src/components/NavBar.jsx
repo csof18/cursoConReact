21cd42328a083cf858c96ee7a127437881874adc
csof18 <sofiacorrado18@gmail.com>
2021-07-29 23:30:33 -0300

    creando menu y logo

---
+++ proyecto-final/src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import { Navbar } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import{NavDropdown} from 'react-bootstrap';
+function  NavBar(){
+    return(
+
+        <Navbar bg="light" expand="lg" className="App-nav">
+            <Container>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav>
+                    <Nav.Link href="#home">Flores</Nav.Link>
+                    <Nav.Link href="#link">Desayunos</Nav.Link>
+                    <Nav.Link href="#link">Globos</Nav.Link>
+                    <NavDropdown title="Combos especiales" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Especiales</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Dia del niño</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Cumpleaños</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+    )
+}
+
+export default NavBar
