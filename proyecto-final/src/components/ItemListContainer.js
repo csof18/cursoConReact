954a1d2e1d80a5dd26edd1cd70b5ac28052e53df
csof18 <sofiacorrado18@gmail.com>
2021-09-06 12:35:22 -0300

    probando firebase

--- proyecto-final/src/components/ItemListContainer.js
+++ proyecto-final/src/components/ItemListContainer.js
@@ -13,18 +13,21 @@ export default function ItemListContainer(){
         //pido los datos
         const getProducto = async () => {
             //acceso a coleccion de productos que cargue
-            const productoCollection = collection(getData(), 'productos');
+            const productoCollection = collection(getData(), 'producto');
             //obtengo el snapShot(muestra la lista en ese momento, si cambia la lista no se actualiza)
             const productoSnapshot = await getDocs(productoCollection);
             //obtengo datos en formato json con data()
+            //docs es el array pero no son los datos en si  sino que es un objeto mucho mas grande
             const productoList = productoSnapshot.docs.map(doc => doc.data());
             console.log(productoList, 'VER QUE DEVUELVE PRODUCTOLIST');
+            setProducto(productoList);
+
             setCargando(true);
             
             
         }
         getProducto();
-    })
+    }, [])
    /* useEffect(()=>{
         new Promise((resolve, reject) => {
             setCargando(true);
